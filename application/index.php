26460fa6e793a6e77744f78de28aae2617d8d206
sergiopaulino <sergio@netants.nl>
2014-10-16 16:50:09 +0200

    index moving

---
+++ application/index.php
@@ -0,0 +1,25 @@
+<?php
+define ('DS', DIRECTORY_SEPARATOR );
+define ('HOME', dirname(__FILE__) );
+
+ini_set ('display_errors', 0);
+
+
+require_once HOME . DS . 'utilities' . DS . 'bootstrap.php';
+
+
+function __autoload($class)
+{
+    if (file_exists(HOME . DS . 'utilities' . DS . ($class) . '.php'))
+    {
+        require_once HOME . DS . 'utilities' . DS . ($class) . '.php';
+    }
+    else if (file_exists(HOME . DS . 'models' . DS . ($class) . '.php'))
+    {
+        require_once HOME . DS . 'models' . DS . ($class) . '.php';
+    }
+    else if (file_exists(HOME . DS . 'controllers' . DS . ($class) . '.php'))
+    {
+        require_once HOME . DS . 'controllers'  . DS . ($class) . '.php';
+    }
+}
